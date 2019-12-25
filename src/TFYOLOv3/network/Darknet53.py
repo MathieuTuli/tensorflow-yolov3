11407f8bc240c7325d84f1bcbf3b910ffaf764e4
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-12-25 10:15:24 -0500

    WIP

--- src/TFYOLOv3/network/Darknet53.py
+++ src/TFYOLOv3/network/Darknet53.py
@@ -1,52 +1,93 @@
 from tensorflow.keras import layers, models
-from typing import Optional, Tuple
+from typing import Union, Tuple
 
 import tensorflow as tf
 import logging
 
+from .helpers import BatchNormalization
+
 
 class Darknet53():
     def __init__(self,) -> None:
-        ...
+        self.model: models.Model = None
 
     def __str__(self) -> str:
         return 'Darknet53'
 
     @staticmethod
-    def conv(filters: int,
-             kernel_size: Optional[int, Tuple[int, int]],
-             strides:  Optional[int, Tuple[int, int]],
-             padding: str = "valid",
+    def conv(prev_layer: layers.Layer,
+             filters: int,
+             kernel_size: Union[int, Tuple[int, int]],
+             strides:  Union[int, Tuple[int, int]] = 1,
              data_format: str = "channels_last",
-             use_bias: bool = False,) -> layers.Layer:
+             batch_norm: bool = True) -> layers.Layer:
+        if strides == 1:
+            padding = 'same'
+        else:
+            # top left half padding
+            prev_layer = layers.ZeroPadding2D(((1, 0), (1, 0)))(prev_layer)
+            padding = 'valid'
         layer = layers.Conv2D(
             filters=filters,
             kernel_size=kernel_size,
             strides=strides,
             padding=padding,
             data_format=data_format,
-            use_bias=use_bias)
+            use_bias=not batch_norm,
+            kernel_regularizer=tf.keras.regularizers.l2(0.0005))(prev_layer)
+        if batch_norm:
+            layer = BatchNormalization()(layer)
+            layer = layers.LeakyReLU(alpha=0.1)(layer)
+
         return layer
 
     @staticmethod
-    def block() -> layers.Layer:
-        ...
+    def block(prev_layer: layers.Layer,
+              filters: int,
+              num_blocks: int) -> layers.Layer:
+        layer = Darknet53.conv(prev_layer=prev_layer,
+                               filters=filters,
+                               kernel_size=3,
+                               strides=2)
+        for i in range(num_blocks):
+            layer = Darknet53.residual(layer, filters)
+        return layer
 
     @staticmethod
-    def residual() -> layers.Layer:
-        ...
+    def residual(
+            prev_layer: layers.Layer,
+            filters: int) -> layers.Layer:
+        res = Darknet53.conv(prev_layer=prev_layer,
+                             filters=filters // 2,
+                             kernel_size=1)
+        res = Darknet53.conv(prev_layer=res,
+                             filters=filters,
+                             kernel_size=3)
+        res = layers.Add()([prev_layer, res])
+        return res
 
-    def build(self, eager: bool = False) -> models.Model:
-        model = models.Sequential()
-        if eager:
-            ...
-        else:
-            model.add(layers.Conv2D(filters=32, kernel_size=(3, 3),
-                                    strides=(1, 1), padding='valid',
-                                    data_format='channels_last',
-                                    activation='',
-                                    input_shape=(256, 256, 3)))
-            model.add(layers.Conv2D(filters=64, kernel_size=(3, 3),
-                                    strides=(2, 2), padding='valid',
-                                    data_format='channels_last',
-                                    activation=''))
+    def build(self) -> models.Model:
+        # channel last
+        layer = inputs = layers.Input([None, None, 3])
+        layer = Darknet53.conv(prev_layer=layer,
+                               filters=32,
+                               kernel_size=3)
+        layer = Darknet53.block(prev_layer=layer,
+                                filters=64,
+                                num_blocks=1)
+        layer = layer_36 = Darknet53.block(prev_layer=layer,
+                                           filters=256,
+                                           num_blocks=8)
+        layer = layer_61 = Darknet53.block(prev_layer=layer,
+                                           filters=512,
+                                           num_blocks=8)
+        layer = Darknet53.block(layer, filters=1024, num_blocks=4)
+        self.model = models.Model(inputs,
+                                  (layer_36, layer_61, layer),
+                                  name=str(self))
+        return self.model
+
+    def summary(self) -> str:
+        if self.model is None:
+            return 'Model is None'
+        return self.model.summary()
