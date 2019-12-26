cfb04a9878aa378f184430b7d9758e6655ce164a
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-12-26 15:50:59 -0500

    Add template YOLOV3 model. Need to workout call and correctness

--- src/TFYOLOv3/network/models.py
+++ src/TFYOLOv3/network/models.py
@@ -14,11 +14,15 @@ class Darknet53(Model):
         https://pjreddie.com/media/files/papers/YOLOv3.pdf
     """
 
-    def __init__(self, input_shape: Tuple[int, int, int],
-                 num_classes: int, backbone: bool = True, **kwargs) -> None:
-        """
-        @param input_shape: requires channels_last specfication
-        """
+    def __init__(self,
+                 num_classes: int = -1,
+                 backbone: bool = True,
+                 **kwargs) -> None:
+        '''
+        @param: num_classes: if backbone is set, must also set
+        @param: backbone: flag to indicate use of Darknet53 as a backbone
+                          feature extractor
+        '''
         super(Darknet53, self).__init__(**kwargs)
 
         logging.info("Darknet53: starting build")
@@ -46,10 +50,33 @@ class Darknet53(Model):
         layer = self.conv1(inputs)
         layer = self.block1(layer)
         layer = self.block2(layer)
-        layer = skip1 = self.block3(layer)
-        layer = skip2 = self.block4(layer)
+        layer = early_stop = self.block3(layer)
+        layer = middle_stop = self.block4(layer)
         layer = self.block5(layer)
         if self.pooled is not None:
             layer = self.pooled(layer)
             layer = self.dense(layer)
-        return (skip1, skip2, layer)
+        return (early_stop, middle_stop, layer)
+
+
+class YOLOv3(Model):
+    def __init__(self,
+                 num_classes: int,
+                 backbone: bool = True,
+                 **kwargs) -> None:
+        super(YOLOv3, self).__init__(**kwargs)
+        self.conv_scale3 = Darknet53Conv(filters=255, kernel_size=1)
+
+        # TODO: add upsample
+        self.conv1_scale2 = Darknet53Conv(filters=256, kernel_size=1)
+        self.conv2_scale2 = Darknet53Conv(filters=255, kernel_size=1)
+        self.block1_scale2 = [layers.Add()[
+            Darknet53Conv(filters=256, kernel_size=1),
+            Darknet53Conv(filters=512, kernel_size=3)] for i in range(3)]
+
+        # TODO: add upsample
+        self.conv1_scale1 = Darknet53Conv(filters=128, kernel_size=1)
+        self.conv2_scale1 = Darknet53Conv(filters=255, kernel_size=1)
+        self.block1_scale1 = [layers.Add()[
+            Darknet53Conv(filters=128, kernel_size=1),
+            Darknet53Conv(filters=256, kernel_size=3)] for i in range(3)]
